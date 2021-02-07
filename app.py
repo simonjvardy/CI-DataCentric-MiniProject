618a7f74416431f9b16427f8280bc306fc3887d8
Simon Vardy <65361514+simonjvardy@users.noreply.github.com>
2021-02-07 23:45:13 +0000

    Allow user to submit task to db - fix bugs

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ def add_task():
             "due_date": request.form.get("due_date"),
             "created_by": session["user"]
         }
-        mongo.db.task.insert_one(task)
+        mongo.db.tasks.insert_one(task)
         flash("Task Successfully Added")
         return redirect(url_for("get_tasks"))
 
